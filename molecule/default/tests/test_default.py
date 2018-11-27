84566c3379cfb6097ae2885683a5fe5270f168fa
mwolff <mathias@celea.org>
2018-11-27 19:14:15 +0100

    version 1

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -13,5 +13,5 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_svc(host, svc):
     service = host.service(svc)
 
-    assert service.is_running
     assert service.is_enabled
+    assert service.is_running
